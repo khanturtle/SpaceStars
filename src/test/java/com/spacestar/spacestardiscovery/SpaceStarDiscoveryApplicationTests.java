484ee80ec07560c16fea63ab167e76ccee634a50
Dahye73 <sindahye730@gmail.com>
2024-05-16 16:44:19 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/com/spacestar/spacestardiscovery/SpaceStarDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacestar.spacestardiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceStarDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
