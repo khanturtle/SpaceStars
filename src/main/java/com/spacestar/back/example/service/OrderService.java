a3f5615130102af8d72e4a740709228dc1fc27e6
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2024-05-16 10:49:21 +0900

    Initial commit

---
+++ src/main/java/com/spacestar/back/example/service/OrderService.java
@@ -0,0 +1,15 @@
+//package com.tyranno.ssg.order.application;
+//
+//import com.tyranno.ssg.order.dto.OrderAddDto;
+//import com.tyranno.ssg.order.dto.OrderListDto;
+//import com.tyranno.ssg.order.dto.ResponseNonOrderDto;
+//
+//import java.util.List;
+//
+//public interface OrderService {
+//    void addOrderList(OrderAddDto orderAddDto, String uuid);
+//
+//    List<OrderListDto> getOrderList(String uuid);
+//
+//    List<OrderListDto> getOrderList(ResponseNonOrderDto responseNonOrderDto);
+//}
