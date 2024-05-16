1529b9924937cc274cdc66295a8effe650825dfa
heeyoung <9hiyoung@gmail.com>
2024-05-16 11:27:33 +0900

    Chore: gitignore 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // (option) serverless dockerlizing을 위해
     unoptimized: true,
   },
-  output: 'standalone', // dockerlizing을 위해
+  outputStandalone: true,
 }
 
 export default nextConfig
