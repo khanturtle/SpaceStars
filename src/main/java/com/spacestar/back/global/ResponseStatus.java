a0f36eeb05908c0e0f204b1fd867165fe90dd2d7
조준호 <81681883+khanturtle@users.noreply.github.com>
2024-05-16 09:30:22 +0900

    Initial commit

---
+++ src/main/java/com/spacestar/back/global/ResponseStatus.java
@@ -0,0 +1,24 @@
+package com.spacestar.back.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum ResponseStatus {
+
+    /**
+     * 200: 요청 성공
+     **/
+    SUCCESS(200, "요청에 성공했습니다."),
+
+    INTERNAL_SERVER_ERROR(900, "Internal server error"),
+
+    TOKEN_NOT_VALID( 2002, "토큰이 유효하지 않습니다.");
+//    TOKEN_NULL(false, 2003, "토큰이 존재하지 않습니다."),
+
+    private final int code;
+    private final String message;
+
+}
---
+++ src/main/java/com/spacestar/back/global/ResponseSuccess.java
@@ -0,0 +1,21 @@
+package com.spacestar.back.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum ResponseSuccess {
+
+    SUCCESS(200, "요청에 성공했습니다."),
+
+    INTERNAL_SERVER_ERROR(900, "Internal server error"),
+
+    TOKEN_NOT_VALID( 2002, "토큰이 유효하지 않습니다.");
+
+
+    private final int code;
+    private final String message;
+
+}
