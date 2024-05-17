917cc2271c8de8ce2b1d686b315373976a4d2bd6
Dahye73 <sindahye730@gmail.com>
2024-05-17 10:04:21 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/com/spacestar/spacestarconfigserver/SpaceStarConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacestar.spacestarconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceStarConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
