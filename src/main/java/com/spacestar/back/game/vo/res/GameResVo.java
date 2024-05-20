ab4bee17c1ca15dcad52d576675fa64aee06eff0
chojunho <chojunho0310@naver.com>
2024-05-20 10:57:16 +0900

    Fix : GameResVo 불필요한 어노테이션 제거

--- src/main/java/com/spacestar/back/game/vo/res/GameResVo.java
+++ src/main/java/com/spacestar/back/game/vo/res/GameResVo.java
@@ -3,10 +3,6 @@ package com.spacestar.back.game.vo.res;
 import lombok.*;
 
 @Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class GameResVo {
     private int index;
     private String gameName;
