1cc0329818ad27b5adafdb43da9f2e2797cbb1db
Dahye73 <sindahye730@gmail.com>
2024-05-20 10:19:10 +0900

    Init : 프로젝트 생성

---
+++ src/main/java/org/spacestar/spacestargateway/SpaceStarGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.spacestar.spacestargateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceStarGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceStarGatewayApplication.class, args);
+	}
+
+}
