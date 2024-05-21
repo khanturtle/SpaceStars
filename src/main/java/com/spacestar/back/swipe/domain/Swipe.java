5ade70bf9581117c7475c27495b3032ffa2300ac
chojunho <chojunho0310@naver.com>
2024-05-21 10:54:03 +0900

    Feat : GlobalTime extend

--- src/main/java/com/spacestar/back/swipe/domain/Swipe.java
+++ src/main/java/com/spacestar/back/swipe/domain/Swipe.java
@@ -1,12 +1,13 @@
 package com.spacestar.back.swipe.domain;
 
+import com.spacestar.back.global.GlobalTime;
 import com.spacestar.back.swipe.SwipeStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class Swipe {
+public class Swipe extends GlobalTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
