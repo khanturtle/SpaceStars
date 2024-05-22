7525d7fcb207e1d0fae9ec17faa0acce10b21f7f
chojunho <chojunho0310@naver.com>
2024-05-22 15:34:56 +0900

    Feat : 추가해야할 기능 메모

--- src/main/java/com/spacestar/back/swipe/service/SwipeServiceImpl.java
+++ src/main/java/com/spacestar/back/swipe/service/SwipeServiceImpl.java
@@ -17,6 +17,7 @@ public class SwipeServiceImpl implements SwipeService{
     @Override
     public void addSwipe(SwipeReqDto swipeReqDto, String uuid) {
         swipeRepository.save(Swipe.toEntity(swipeReqDto,uuid));
+        //Todo 요청을 보내면 추천인 목록에서 제외 시켜야함 (거절시 처럼 Redis에 저장해야할듯?)
     }
 
     @Override
@@ -27,10 +28,12 @@ public class SwipeServiceImpl implements SwipeService{
     @Override
     public void agreeSwipe(String uuid) {
         swipeRepository.agreeRequest(uuid);
+        //Todo 수락시 채팅방 생성되어야 함
     }
 
     @Override
     public void rejectSwipe(String uuid) {
         swipeRepository.rejectRequest(uuid);
+        //Todo 거절시 Redis에 해당 유저 저장해야함
     }
 }
