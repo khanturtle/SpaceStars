15188f1f1d7ad6510469c07faa50eefbb14e2b80
heeyoung <9hiyoung@gmail.com>
2024-05-24 17:18:31 +0900

    Feat: SVG Component 생성

---
+++ packages/ui/src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CloseIcon } from './Close'
+export { default as CheckIcon } from './Check'
+export { default as BotIcon } from './Bot'
+export { default as CalendarIcon } from './Calendar'
+export { default as ArrowIcon } from './Arrow'
