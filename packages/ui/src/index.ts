e99c8ff968ad39127405d3e276071da7b6d169b8
heeyoung <9hiyoung@gmail.com>
2024-05-27 17:07:08 +0900

    Feat: 게임 선택 버튼

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,4 @@
-export { Button, KakaoButton } from './components/Buttons'
+export { Button, KakaoButton, GameButton } from './components/Buttons'
 export { Input, Checkbox, Select } from './components/Inputs'
 export {
   CloseIcon,
