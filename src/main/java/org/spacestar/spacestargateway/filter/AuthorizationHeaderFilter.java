74264e2acd59f0367ea94f619c96d94a61edf405
Dahye73 <sindahye730@gmail.com>
2024-05-28 01:04:23 +0900

    Feat : jwt 토큰 검증 필터 구현

--- src/main/java/org/spacestar/spacestargateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/org/spacestar/spacestargateway/filter/AuthorizationHeaderFilter.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import io.jsonwebtoken.Jwts;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
@@ -32,13 +33,41 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 	@Override
 	public GatewayFilter apply(Config config){
 
-		return ((exchange, chain) -> {
-			String token = exchange.getRequest().getHeaders().get("Authorization").get(0).substring(7); // 헤더의 토큰 파싱 (Bearer 제거)
-			Map<String, Object> userInfo = jwtUtil.
+		return (exchange, chain) -> {
 
-		})
+			HttpHeaders headers = exchange.getRequest().getHeaders();
+			if(!headers.containsKey("Authorization")){
+				return onError(exchange, "토큰이 없습니다.", HttpStatus.UNAUTHORIZED);
+			}
+
+			String token = headers.get("Authorization").get(0).substring(7);
+
+			try{
+				String userUuid = jwtUtil.getUuid(token);
+				ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+						.header("uuid", userUuid)
+						.build();
+				ServerWebExchange mutatedExchange = exchange.mutate().request(mutatedRequest).build();
+				return chain.filter(mutatedExchange);
+			}catch(IllegalStateException e) {
+				HttpStatus status = e.getMessage().equals("만료된 토큰입니다.") ? HttpStatus.FORBIDDEN : HttpStatus.UNAUTHORIZED;
+				return onError(exchange, e.getMessage(), status);
+			}
+		};
 	}
 
+	private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus){
+
+		ServerHttpResponse response = exchange.getResponse();
+		response.setStatusCode(httpStatus);
+		return response.setComplete();
+	}
+	// jwt 검증을 성공한 요청의 헤더에 uuid 추가
+	private void addAuthorizationHeaders(ServerHttpRequest request, String userUuid){
+		request.mutate()
+				.header("uuid" , userUuid)
+				.build();
+	}
 	public class Config {
 	}
 }
--- src/main/java/org/spacestar/spacestargateway/jwt/JwtUtil.java
+++ src/main/java/org/spacestar/spacestargateway/jwt/JwtUtil.java
@@ -2,6 +2,7 @@ package org.spacestar.spacestargateway.jwt;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -10,24 +11,45 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
 
+	@Value("${jwt.secret}")
 	private SecretKey secretKey;
 
+
+	// jwt 에서 클레임 추출
+	private Claims parseClaims(String accessToken){
+
+		try{
+			return Jwts.parser()
+					.verifyWith(secretKey)
+					.build()
+					.parseSignedClaims(accessToken)
+					.getPayload();
+		} catch (ExpiredJwtException e){
+			throw new IllegalStateException("만료된 토큰입니다.");
+		} catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e){
+			throw new IllegalStateException("유효하지 않은 토큰입니다.");
+		}
+	}
+
+	// 토큰을 파싱하여 클레임 객체 반환 -> 반환된 클레임 객체에서 uuid 추출
 	public String getUuid(String token){
 		return parseClaims(token).get("uuid", String.class);
 	}
 
-	private Claims parseClaims(String accessToken){
+	// role 클레임 추출
+	public String getRole(String token) {
 
-		try{
-			return Jwts.parser().setSigningKey(secretKey).build().parseClaimsJws(accessToken).getBody();
-		} catch (ExpiredJwtException e){
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+	}
 
-		} catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e){
+	// 토큰 만료 여부 확인
+	public Boolean isExpired(String token) {
 
-		}
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
 	}
 }
