a29219ad87d52947f828c6491bd6df7b2e43f726
heeyoung <9hiyoung@gmail.com>
2024-05-28 15:38:23 +0900

    Feat: shadcn datepicker 추가

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-import path from 'path'
-
 const nextConfig = {
   experimental: {
     externalDir: true,
   },
-  // webpack: (config) => {
-  //   config.resolve.alias['@'] = path.resolve('./src')
-  //   return config
-  // },
 }
 
 export default nextConfig
