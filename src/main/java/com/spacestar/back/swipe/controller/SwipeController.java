868a9228bc0f32e3b2386175b910eedee1ebcb59
chojunho <chojunho0310@naver.com>
2024-05-28 16:11:30 +0900

    Refactor : 보낸 요청 uri 수정

--- src/main/java/com/spacestar/back/swipe/controller/SwipeController.java
+++ src/main/java/com/spacestar/back/swipe/controller/SwipeController.java
@@ -19,41 +19,42 @@ import java.util.List;
 public class SwipeController {
     private final SwipeService swipeService;
     private final ModelMapper mapper;
+
     @PostMapping("/add")
     public ResponseEntity<Void> addSwipe(@RequestHeader("UUID") String uuid,
-                                         @RequestBody SwipeReqVo swipeReqVo){
-        swipeService.addSwipe(mapper.map(swipeReqVo, SwipeReqDto.class),uuid);
+                                         @RequestBody SwipeReqVo swipeReqVo) {
+        swipeService.addSwipe(mapper.map(swipeReqVo, SwipeReqDto.class), uuid);
         return new ResponseEntity<>(ResponseSuccess.SWIPE_ADD_SUCCESS);
     }
 
-    @GetMapping
-    public ResponseEntity<List<SwipeListResVo>> getSwipe(@RequestHeader("UUID") String uuid){
+    @GetMapping("/received")
+    public ResponseEntity<List<SwipeListResVo>> getSwipe(@RequestHeader("UUID") String uuid) {
         List<SwipeListResDto> swipeListResDtos = swipeService.getReceivedSwipe(uuid);
         List<SwipeListResVo> SwipeListResVos = swipeListResDtos.stream()
                 .map(dto -> mapper.map(dto, SwipeListResVo.class))
                 .toList();
 
-        return new ResponseEntity<>(ResponseSuccess.SWIPE_GET_SUCCESS,SwipeListResVos);
+        return new ResponseEntity<>(ResponseSuccess.SWIPE_GET_SUCCESS, SwipeListResVos);
     }
 
     @GetMapping("/sent")
-    public ResponseEntity<List<SwipeListResVo>> getSentSwipe(@RequestHeader("UUID") String uuid){
+    public ResponseEntity<List<SwipeListResVo>> getSentSwipe(@RequestHeader("UUID") String uuid) {
         List<SwipeListResDto> swipeListResDtos = swipeService.getSentSwipe(uuid);
         List<SwipeListResVo> SwipeListResVos = swipeListResDtos.stream()
                 .map(dto -> mapper.map(dto, SwipeListResVo.class))
                 .toList();
 
-                return new ResponseEntity<>(ResponseSuccess.SWIPE_GET_SUCCESS,SwipeListResVos);
+        return new ResponseEntity<>(ResponseSuccess.SWIPE_SENT_GET_SUCCESS, SwipeListResVos);
     }
 
     @PatchMapping("/agree")
-    public ResponseEntity<Void> agreeSwipe(@RequestHeader("UUID") String uuid){
+    public ResponseEntity<Void> agreeSwipe(@RequestHeader("UUID") String uuid) {
         swipeService.agreeSwipe(uuid);
         return new ResponseEntity<>(ResponseSuccess.SWIPE_AGREE_SUCCESS);
     }
 
     @PatchMapping("/reject")
-    public ResponseEntity<Void> rejectSwipe(@RequestHeader("UUID") String uuid){
+    public ResponseEntity<Void> rejectSwipe(@RequestHeader("UUID") String uuid) {
         swipeService.rejectSwipe(uuid);
         return new ResponseEntity<>(ResponseSuccess.SWIPE_REJECT_SUCCESS);
     }
