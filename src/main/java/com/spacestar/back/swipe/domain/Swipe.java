d7637f215e49851848a22aafcf7543156ae81602
chojunho <chojunho0310@naver.com>
2024-05-28 16:04:06 +0900

    Refactor : swipe 엔티티 수정

--- src/main/java/com/spacestar/back/swipe/domain/Swipe.java
+++ src/main/java/com/spacestar/back/swipe/domain/Swipe.java
@@ -2,25 +2,34 @@ package com.spacestar.back.swipe.domain;
 
 import com.spacestar.back.global.GlobalTime;
 import com.spacestar.back.swipe.SwipeStatus;
-import com.spacestar.back.swipe.dto.req.SwipeReqDto;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class Swipe extends GlobalTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String matchToMember;
+    @Column(nullable = false)
     private String matchFromMember;
+    @Column(length = 30)
+    private String memo;
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private SwipeStatus status;
-    private String memo;
+
+    @Builder
+    public Swipe(Long id, String matchToMember, String matchFromMember, String memo, SwipeStatus status) {
+        this.id = id;
+        this.matchToMember = matchToMember;
+        this.matchFromMember = matchFromMember;
+        this.memo = memo;
+        this.status = status;
+    }
 }
