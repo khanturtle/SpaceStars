7da23879381740def41959861806649829ac5cc7
Dahye73 <sindahye730@gmail.com>
2024-06-03 10:23:41 +0900

    Chore : add CorsConfig file

---
+++ src/main/java/com/spacestar/back/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.spacestar.back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("http://localhost:8000", "http://15.165.68.220:8000", "http://spacestars.kr:8000")
+					.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+					.allowedHeaders("*")
+					.allowCredentials(true);
+			}
+		};
+	}
+}
