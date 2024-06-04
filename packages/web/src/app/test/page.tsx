994b5ee1f733b91d9f08a14a94642215f26c4bfb
heeyoung <9hiyoung@gmail.com>
2024-06-04 10:59:57 +0900

    Feat: 로그인 테스트

--- packages/web/src/app/test/page.tsx
+++ packages/web/src/app/test/page.tsx
@@ -1,14 +1,8 @@
-import DatePickerCustom from '@/components/DatePicker/DatePicker'
 import TeamBox from '@/containers/team-list/TeamBox'
 
 export default function page() {
   return (
     <div>
-      <div>
-        <h1>데이트피커</h1>
-        <DatePickerCustom id="" />
-      </div>
-
       <div>
         <h1>팀원찾기</h1>
         <TeamBox>ㅇㅅㅇ</TeamBox>
--- packages/web/src/components/ui/calendar.tsx
+++ packages/web/src/components/ui/calendar.tsx
@@ -10,8 +10,8 @@ import { buttonVariants } from './button'
 import { cn } from '@/lib/utils'
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker> & {
-  className?: string
-  classNames?: string
+  className?: unknown
+  classNames?: unknown
   showOutsideDays?: boolean
 }
 
