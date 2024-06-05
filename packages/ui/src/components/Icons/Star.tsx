6d21689517a8880ded100c6f46354c32e9f8bfe4
heeyoung <9hiyoung@gmail.com>
2024-06-05 16:45:38 +0900

    Style: svg 속성 jsx 문법에 맞게 수정

--- packages/ui/src/components/Icons/Star.tsx
+++ packages/ui/src/components/Icons/Star.tsx
@@ -22,8 +22,8 @@ const Star = ({
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
         fill={fill}
       />
