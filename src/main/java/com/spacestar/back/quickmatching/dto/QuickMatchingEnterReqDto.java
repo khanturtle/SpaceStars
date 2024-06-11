62b8b64abbdab2d09450be8e28a2c2f1909b5503
chojunho <chojunho0310@naver.com>
2024-06-11 21:02:41 +0900

    Refactor : Remove annotation

--- src/main/java/com/spacestar/back/quickmatching/dto/QuickMatchingEnterReqDto.java
+++ src/main/java/com/spacestar/back/quickmatching/dto/QuickMatchingEnterReqDto.java
@@ -1,10 +1,8 @@
 package com.spacestar.back.quickmatching.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class QuickMatchingEnterReqDto {
     private String gameName;
 }
