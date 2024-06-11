aa9407623ef5da48e667bfc44d7e6f7dc2ee1f2c
heeyoung <9hiyoung@gmail.com>
2024-06-11 14:36:34 +0900

    Feat: game API fetcing

--- packages/web/src/app/dashboard/team-list/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function layout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <section className="flex-1">{children}</section>
-}
---
+++ packages/web/src/containers/team-list/SelectBoxContainer.tsx
@@ -0,0 +1,45 @@
+import { GameType } from '@/apis/game'
+import { LockIcon } from '@packages/ui'
+import GameSelectBox from './GameSelectBox'
+import SubSelectBox from './SubSelectBox'
+
+export default function SelectBoxContainer({
+  searchParams,
+  games,
+}: {
+  searchParams: { [key: string]: string }
+  games: GameType[]
+}) {
+  return (
+    <div>
+      <GameSelectBox games={games} searchParams={searchParams} />
+
+      <div className="h-[20px]" />
+
+      <SubSelectBox>
+        <SubSelectBox.Title
+          title="Team List"
+          // FIXME: Room 갯수로 수정
+          desc={`(${games.length} Items)`}
+        />
+        <div className="flex-1" />
+        <SubSelectBox.Search />
+        {/* FIXME: query key-value 수정 */}
+        <SubSelectBox.SubButton
+          name="모집중"
+          queryKey="test1"
+          queryValue="1"
+          searchParams={searchParams}
+        />
+        <SubSelectBox.SubButton
+          name="lock"
+          icon={<LockIcon fill="#2C3149" />}
+          queryKey="test2"
+          queryValue="2"
+          searchParams={searchParams}
+        />
+        <SubSelectBox.ViewToggle searchParams={searchParams} />
+      </SubSelectBox>
+    </div>
+  )
+}
