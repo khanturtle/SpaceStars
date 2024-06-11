5dd9107db2a969e22b80c437c1362033aef4bd60
heeyoung <9hiyoung@gmail.com>
2024-06-11 11:41:41 +0900

    Feat: 추가정보 모달 패딩 수정

--- packages/web/src/app/@modal/(.)additional-details/page.tsx
+++ packages/web/src/app/@modal/(.)additional-details/page.tsx
@@ -9,7 +9,7 @@ export default function page({
   return (
     <Modal>
       <AdditionalDetailsLayout
-        className="relative h-full pt-[90px] pb-[60px] flex flex-col items-center"
+        className="relative h-full px-[204px] pt-[90px] pb-[60px] flex flex-col items-center"
         step={searchParams.step}
       />
     </Modal>
--- packages/web/src/containers/additional-details/LikeGameForm.tsx
+++ packages/web/src/containers/additional-details/LikeGameForm.tsx
@@ -5,12 +5,18 @@ import { ItemType, GAME_DATA } from './state'
 export default function LikeGameForm({
   selectedGames,
   onClick,
+  className,
+  ...props
 }: {
   selectedGames: ItemType[]
   onClick: (game: ItemType) => void
+  className?: string
 }) {
   return (
-    <section className="flex flex-row flex-wrap justify-between gap-[17px_0] mb-[47px]">
+    <section
+      className={`flex flex-row flex-wrap justify-between gap-[17px_0] mb-[47px] ${className}`}
+      {...props}
+    >
       {GAME_DATA.map((item) => (
         <GameButton
           key={item.index}
