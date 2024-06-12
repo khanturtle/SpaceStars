557668f6450f6d22f33e33270556ddec7bbe4364
chojunho <chojunho0310@naver.com>
2024-06-12 16:39:52 +0900

    Refactor : QuickMatchingController Response

--- src/main/java/com/spacestar/back/quickmatching/controller/QuickMatchingController.java
+++ src/main/java/com/spacestar/back/quickmatching/controller/QuickMatchingController.java
@@ -1,5 +1,7 @@
 package com.spacestar.back.quickmatching.controller;
 
+import com.spacestar.back.global.ResponseEntity;
+import com.spacestar.back.global.ResponseSuccess;
 import com.spacestar.back.quickmatching.dto.QuickMatchingEnterReqDto;
 import com.spacestar.back.quickmatching.service.QuickMatchingService;
 import com.spacestar.back.quickmatching.vo.QuickMatchingEnterReqVo;
@@ -8,11 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
 
 @RestController
 @Tag(name = "Queue", description = "빠른 매칭(큐)")
@@ -26,29 +24,26 @@ public class QuickMatchingController {
     public ResponseEntity<Void> enterQuickMatching(@RequestHeader("UUID")String uuid,
                                                    @RequestBody QuickMatchingEnterReqVo reqVo){
         quickMatchingService.enterQuickMatching(uuid, mapper.map(reqVo, QuickMatchingEnterReqDto.class));
-        return null;
-    }
-    @Operation(summary = "대기 큐 SSE 연결")
-    @GetMapping(value="/connect", produces= MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> connect(@RequestParam("gameName") String gameName){
-        SseEmitter emitter = quickMatchingService.connect(gameName);
-        return ResponseEntity.ok(emitter);
+        return new ResponseEntity<>(ResponseSuccess.SUCCESS);
     }
     @Operation(summary = "큐 요청 수락하기")
     @PatchMapping("/accept")
-    public void acceptQuickMatch(@RequestHeader("UUID")String uuid) {
+    public ResponseEntity<Void> acceptQuickMatch(@RequestHeader("UUID")String uuid) {
         quickMatchingService.acceptQuickMatch(uuid);
+        return new ResponseEntity<>(ResponseSuccess.SUCCESS);
     }
     @Operation(summary = "큐 요청 거절하기")
     @PatchMapping("/reject")
-    public void rejectQuickMatch(@RequestHeader("UUID")String uuid) {
+    public ResponseEntity<Void> rejectQuickMatch(@RequestHeader("UUID")String uuid) {
         quickMatchingService.rejectQuickMatch(uuid);
+        return new ResponseEntity<>(ResponseSuccess.SUCCESS);
     }
     @Operation(summary = "수락 큐 결과")
     @PostMapping("/complete")
-    public QuickMatchingResVo completeQuickMatch(@RequestHeader("UUID")String uuid,
+    public ResponseEntity<QuickMatchingResVo> completeQuickMatch(@RequestHeader("UUID")String uuid,
                                                  @RequestBody QuickMatchingEnterReqVo reqVo){
-        return mapper.map(quickMatchingService.completeQuickMatch(uuid,mapper.map(reqVo, QuickMatchingEnterReqDto.class)), QuickMatchingResVo.class);
+        return new ResponseEntity<>(ResponseSuccess.SUCCESS,
+                mapper.map(quickMatchingService.completeQuickMatch(uuid,mapper.map(reqVo, QuickMatchingEnterReqDto.class)), QuickMatchingResVo.class));
     }
 
 }
