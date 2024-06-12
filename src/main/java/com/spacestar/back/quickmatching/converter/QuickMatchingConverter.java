8a5dfd5fdc881dd7802014b3f41009cf5b1e71da
chojunho <chojunho0310@naver.com>
2024-06-12 15:32:01 +0900

    Feat : QuickMatchingConverter

---
+++ src/main/java/com/spacestar/back/quickmatching/converter/QuickMatchingConverter.java
@@ -0,0 +1,17 @@
+package com.spacestar.back.quickmatching.converter;
+
+import com.spacestar.back.quickmatching.domain.QuickMatchStatus;
+import com.spacestar.back.quickmatching.domain.QuickMatching;
+
+public class QuickMatchingConverter {
+
+    public QuickMatching toEntity(String matchFromMember,String matchToMember) {
+        return QuickMatching.builder()
+                .id(matchFromMember + matchToMember)
+                .matchFromMember(matchFromMember)
+                .matchToMember(matchToMember)
+                .matchToMemberStatus(QuickMatchStatus.WAIT)
+                .matchFromMemberStatus(QuickMatchStatus.WAIT)
+                .build();
+    }
+}
