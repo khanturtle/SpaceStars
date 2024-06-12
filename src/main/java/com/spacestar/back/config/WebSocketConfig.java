2720dd5dbc8b0ca165198521f52754c2dbe60294
myonjin <109326291+myonjin@users.noreply.github.com>
2024-06-12 17:35:45 +0900

    Update WebSocketConfig.java

--- src/main/java/com/spacestar/back/config/WebSocketConfig.java
+++ src/main/java/com/spacestar/back/config/WebSocketConfig.java
@@ -19,9 +19,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/v1/wschat");   //SockJS 연결 주소
         registry.addEndpoint("/api/v1/wschat")   //SockJS 연결 주소
-                .withSockJS(); //버전 낮은 브라우    저에서도 적용 가능
+                .setAllowedOriginPatterns("*");  //CORS 허용
+        registry.addEndpoint("/api/v1/wschat")   //SockJS 연결 주소
+                .setAllowedOriginPatterns("*")  //CORS 허용
+                .withSockJS();
     }
 
 }
