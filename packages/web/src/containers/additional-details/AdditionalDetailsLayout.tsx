8644076d7ad0d0c25cceb77bd79360ef12435bdf
heeyoung <9hiyoung@gmail.com>
2024-06-12 17:51:51 +0900

    Feat: 모달 UI 이식

--- packages/web/src/containers/additional-details/AdditionalDetailsLayout.tsx
+++ packages/web/src/containers/additional-details/AdditionalDetailsLayout.tsx
@@ -1,4 +1,8 @@
-import AdditionalDetailsContainer from "./AdditionalDetailsContainer"
+'use client'
+
+import { ModalContext } from '@/components/providers/modal-provider'
+import { useContext } from 'react'
+import AdditionalDetailsContainer from './AdditionalDetailsContainer'
 
 // TODO: 다음 버튼 유효성 검증 및 disabled
 export default function AdditionalDetailsLayout({
@@ -9,8 +13,21 @@ export default function AdditionalDetailsLayout({
   step: string
 }) {
   return (
-    <div className={className}>
-    <AdditionalDetailsContainer  step={step} />
+    <div className="relative h-full px-[204px] pt-[90px] pb-[60px] flex flex-col items-center">
+      <AdditionalDetailsContainer step={step} />
     </div>
+  )
+}
+
+export const DevModalOpenBtn = () => {
+  const { openModal } = useContext(ModalContext)
+
+  return (
+    <button
+      type="button"
+      onClick={() => openModal(<AdditionalDetailsLayout step="1" />)}
+    >
+      추가정보
+    </button>
   )
 }
