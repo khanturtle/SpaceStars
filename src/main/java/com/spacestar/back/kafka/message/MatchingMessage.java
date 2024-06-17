4471287300ce168ec78f50f485a040f8a43625f5
Dahye73 <sindahye730@gmail.com>
2024-06-17 16:24:36 +0900

    Feat : Kafka message 수신

--- src/main/java/com/spacestar/back/kafka/message/MatchingMessage.java
+++ src/main/java/com/spacestar/back/kafka/message/MatchingMessage.java
@@ -1,4 +1,19 @@
 package com.spacestar.back.kafka.message;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MatchingMessage {
+
+	private String senderUuid;
+	private String receiverUuid;
+	private String content;
 }
