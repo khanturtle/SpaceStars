2de574fa193ee49c35ce8849dbfc2d8f80caafcf
heeyoung <9hiyoung@gmail.com>
2024-06-17 20:05:27 +0900

    Fix: images domain 수정

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: [
       't1.kakaocdn.net',
-      's3-alpha-sig.figma.com',
-      'picsum.photos',
-      'opgg-static.akamaized.net',
-      's-valorant-api.op.gg',
       'space-star-bucket.s3.ap-northeast-2.amazonaws.com',
     ],
   },
