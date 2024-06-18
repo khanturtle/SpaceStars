7751873ccad9f084c48bb240b1787f6719442586
heeyoung <9hiyoung@gmail.com>
2024-06-18 09:48:11 +0900

    Refactor: async 제거

--- packages/web/src/app/dashboard/chat/page.tsx
+++ packages/web/src/app/dashboard/chat/page.tsx
@@ -1,4 +1,4 @@
-export default async function page() {
+export default function page() {
   // FIXME: 아무것도 없을 때
   return (
     <div className="flex-1 bg-[purple] w-full h-[calc(100vh_-_100px)] relative">
