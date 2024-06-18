cf21c3bf82fd295f256bd9e036ac32370fc13f42
22gamin <gamin3182@gmail.com>
2024-06-18 11:34:13 +0900

    refactor: SwaggerConfig

--- src/main/java/com/spacestar/back/config/SwaggerConfig.java
+++ src/main/java/com/spacestar/back/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.spacestar.back.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -17,7 +22,14 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .addServersItem(new Server().url("/"));
     }
 }
