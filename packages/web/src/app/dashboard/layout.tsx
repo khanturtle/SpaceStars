94f828ce34cd71cb9aebbc493443f5602d4e0dd7
구희영 <9hiyoung@gmail.com>
2024-06-19 03:31:01 +0900

    Comment: 주석 제거

--- packages/web/src/app/dashboard/layout.tsx
+++ packages/web/src/app/dashboard/layout.tsx
@@ -10,9 +10,9 @@ export default async function layout({
 }: {
   children: React.ReactNode
 }) {
-  const session = await getServerSession(options)
+  // const session = await getServerSession(options)
 
-  console.log("여긴 대시보드 레이아웃", session)
+  // console.log("여긴 대시보드 레이아웃", session)
 
   return (
     <main className="flex w-full h-[calc(100vh_-_100px)] overflow-hidden mx-auto my-0">
