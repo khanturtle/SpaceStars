73342fc75e57b96a1b8c75112b00388bce4e0801
22gamin <gamin3182@gmail.com>
2024-06-19 17:15:47 +0900

    refactor: delete

--- src/main/java/com/spacestar/back/friend/service/FriendService.java
+++ src/main/java/com/spacestar/back/friend/service/FriendService.java
@@ -4,11 +4,7 @@ import com.spacestar.back.friend.dto.req.FriendUuidReqDto;
 import com.spacestar.back.friend.dto.res.FriendListResDto;
 import com.spacestar.back.friend.dto.res.FriendNowResDto;
 import com.spacestar.back.friend.dto.res.FriendRequestResDto;
-<<<<<<< HEAD
-import com.spacestar.back.friend.vo.res.FriendNowResVo;
-=======
 import com.spacestar.back.friend.dto.res.IsFriendResDto;
->>>>>>> a15bd3f460bdbbd3084933c92baefa50dbd98ec2
 
 public interface FriendService {
     void addFriend(String uuid, FriendUuidReqDto friendUuidReqDto);
@@ -21,9 +17,9 @@ public interface FriendService {
 
     void rejectFriend(String uuid, FriendUuidReqDto friendUuidReqDto);
 
-<<<<<<< HEAD
     FriendNowResDto isFriendRequest(String uuid, String targetUuid);
-=======
+
+
     IsFriendResDto isFriend(String uuid, String targetUuid);
->>>>>>> a15bd3f460bdbbd3084933c92baefa50dbd98ec2
+
 }
