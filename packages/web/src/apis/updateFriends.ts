2aa982d2dce4e7387f06aff0d21a44d8258577b0
heeyoung <9hiyoung@gmail.com>
2024-06-19 20:32:25 +0900

    Feat: 회원 정보 전체 조회 및 친구 상태 확인

--- packages/web/src/apis/updateFriends.ts
+++ packages/web/src/apis/updateFriends.ts
@@ -1,16 +1,17 @@
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL_V1}/friend`
 
+// TODO: api 채워넣기
 /** 친구 요청 */
-export async function sendFriendRequest(friendId) {
+export async function sendFriendRequest() {
   try {
   } catch {}
 }
 
 /** 친구 요청 취소 */
-export async function cancelFriendRequest(friendId) {}
+export async function cancelFriendRequest() {}
 
 /** 친구 요청 거절 */
-export async function rejectFriendRequest(friendId) {}
+export async function rejectFriendRequest() {}
 
 /** 친구 요청 수락 */
-export async function acceptFriendRequest(friendId) {}
+export async function acceptFriendRequest() {}
