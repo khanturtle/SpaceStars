494df92456ebdf05433e2c6c493e68696e554423
chojunho <chojunho0310@naver.com>
2024-06-25 14:05:35 +0900

    Fix : SwaggerConfig

--- src/main/java/com/spacestar/back/config/SwaggerConfig.java
+++ src/main/java/com/spacestar/back/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.spacestar.back.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -16,7 +21,14 @@ import io.swagger.v3.oas.models.servers.Server;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .addServersItem(new Server().url("/"));
     }
 }
