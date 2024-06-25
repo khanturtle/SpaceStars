438bf3ae7b917659aff4be917b7ea53a9eb75c52
Dahye73 <sindahye730@gmail.com>
2024-06-25 20:25:44 +0900

    Fix : cors 에러 해결

--- src/main/java/com/spacestar/back/alarm/controller/SseController.java
+++ src/main/java/com/spacestar/back/alarm/controller/SseController.java
@@ -1,6 +1,7 @@
 package com.spacestar.back.alarm.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +25,10 @@ public class SseController {
 
 	private final AlarmServiceImpl alarmService;
 	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@CrossOrigin(origins = "http://localhost:3000")
 	@Operation(summary = "실시간 알림 SSE 입장")
 	public Flux<Message> matchingEvents(@RequestHeader("UUID") String uuid) {
 		log.info("연결시도");
-
 		return alarmService.streamAlarms(uuid);
 	}
 }
