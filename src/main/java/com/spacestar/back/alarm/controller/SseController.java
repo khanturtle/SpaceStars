eac5463645de3e7d32be8eecbd8fcc50cd2e362f
Dahye73 <sindahye730@gmail.com>
2024-06-25 16:28:40 +0900

    Refactor : sse 컨트롤러 분리

---
+++ src/main/java/com/spacestar/back/alarm/controller/SseController.java
@@ -0,0 +1,32 @@
+package com.spacestar.back.alarm.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spacestar.back.alarm.service.AlarmServiceImpl;
+import com.spacestar.back.kafka.message.Message;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sse")
+@Tag(name = "Alarm-SSE", description = "SSE 연결")
+public class SseController {
+
+	private final AlarmServiceImpl alarmService;
+	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@Operation(summary = "실시간 알림 SSE 입장")
+	public Flux<Message> matchingEvents(@RequestHeader("UUID") String uuid) {
+
+		return alarmService.streamAlarms(uuid);
+	}
+}
