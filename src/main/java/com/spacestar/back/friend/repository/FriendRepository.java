9983d03a71c8a1358669fa9b09884149b1dba227
22gamin <gamin3182@gmail.com>
2024-06-27 13:22:00 +0900

    error: git conflict

--- src/main/java/com/spacestar/back/friend/repository/FriendRepository.java
+++ src/main/java/com/spacestar/back/friend/repository/FriendRepository.java
@@ -10,13 +10,8 @@ import java.util.Optional;
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     List<Friend> findByUuid(String uuid);
-
-<<<<<<< Updated upstream
-=======
     List<Friend> findByUuidAndFriendType(String uuid, FriendType friendType);
 
-    @Query("select f from Friend f where f.friendUuid = :friendUuid and f.uuid = :uuid")
->>>>>>> Stashed changes
     Friend findByFriendUuidAndUuid(String friendUuid, String uuid);
 
     Optional<Friend> findByUuidAndFriendUuid(String targetUuid, String uuid);
