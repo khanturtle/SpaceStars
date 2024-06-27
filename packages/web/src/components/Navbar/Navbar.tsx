dfdfe8990fc297f8eaee6b9d06ab83d5f76802b9
heeyoung <9hiyoung@gmail.com>
2024-06-27 10:03:45 +0900

    Style: 전역 컬러 수정 및 로그인 수정

--- packages/web/src/components/Navbar/Navbar.tsx
+++ packages/web/src/components/Navbar/Navbar.tsx
@@ -26,11 +26,15 @@ export default function Navbar({
   profileImageUrl: string | null | undefined
 }) {
   const pathName = usePathname()
-  const noSearchBoxPage = ['/dashboard/queue', '/dashboard/swipe']
+  const noSearchBoxPage = [
+    '/dashboard/swipe',
+    '/dashboard/queue',
+    '/dashboard/queue/start',
+  ]
 
   return (
-    <header>
-      <nav className="h-[100px] w-full flex flex-row items-center bg-[color:var(--White-50,#fff)] sticky z-[1000] px-[54px] py-5 border-b-[#ddd] border-b border-solid left-0 top-0">
+    <header className="bg-[color:var(--nav-color)] border-b-[color:var(--nav-border)] border-b border-solid">
+      <nav className="h-[100px] w-full flex flex-row items-center sticky z-[1000] px-[54px] py-5  left-0 top-0">
         {/* Left */}
         <div className="w-[210px]">
           <Link
--- packages/web/src/components/Navbar/NavbarItem.tsx
+++ packages/web/src/components/Navbar/NavbarItem.tsx
@@ -2,12 +2,18 @@
 
 import { useRouter } from 'next/navigation'
 
+import styles from './navbar.module.css'
+
 export const LoginButton = () => {
   const router = useRouter()
 
   return (
     // TODO: 버튼 커스텀
-    <button type="button" onClick={() => router.push('/sign-in')}>
+    <button
+      type="button"
+      onClick={() => router.push('/sign-in')}
+      className={styles['login-button']}
+    >
       로그인 | 회원가입
     </button>
   )
