0691ae8e741ed5440478b83c6b91607dd72f9070
heeyoung <9hiyoung@gmail.com>
2024-07-02 17:55:39 +0900

    Fix: image 경로 추가

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -13,6 +13,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'spacestars.kr',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
